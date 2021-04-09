4e17d03060fe79d365f05b4a6c55a27e524c8505
minlaxz <minminlaxz@gmail.com>
2021-04-09 20:40:49 +0630

    indexview app is configured.

--- indexview/views.py
+++ indexview/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
+
+
+def indexView(req):
+    res = requests.get("https://minlaxz-bundle.firebaseio.com/public-notes.json")
+    data = res.json().values()
+    # data = [1, 2, 3, 4, 5]
+    return render(req, "indexview/index.html", {"data": data})
